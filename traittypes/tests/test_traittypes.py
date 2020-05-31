b6a897638055100301fe0ee8d7e4d7d9390e5070
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-05-30 17:44:48 -0700

    Remove _bad_values which is not validated and make traitlets fail
    This make upstream test fail as _bad_value generate test that are
    supposed to fail assignments.

--- traittypes/tests/test_traittypes.py
+++ traittypes/tests/test_traittypes.py
@@ -27,7 +27,6 @@ class TestIntArray(TraitTestBase):
     obj = IntArrayTrait()
 
     _good_values = [1, [1, 2, 3], [[1, 2, 3], [4, 5, 6]], np.array([1])]
-    _bad_values = [[1, [0, 0]]]
 
     def assertEqual(self, v1, v2):
         return np.testing.assert_array_equal(v1, v2)
